f353f6ae7927195152af4e728f0e84de04772b55
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-03-03 08:18:26 -0500

    implement plant object

--- app/models.py
+++ app/models.py
@@ -21,3 +21,33 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class Plant(db.Model):
+    """Represents a Plant object"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    species = db.Column(db.String(120))
+    date_acquired = db.Column(db.DateTime, default=datetime.utcnow)
+    location = db.Column(db.String(120))
+    watering_frequency = db.Column(db.Integer)
+    last_watered = db.Column(db.DateTime, nullable=True)
+    notes = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    care_logs = db.relationship("CareLog", backref="plant", lazy="dynamic")
+
+    def __repr__(self):
+        return f"<Plant {self.name}>"
+
+    def needs_water(self):
+        """Checks if plant needs watering based on frequency"""
+        if not self.last_watered:
+            return True
+        days_since = (datetime.utcnow() - self.last_watered).days
+        return days_since >= self.watering_frequency
