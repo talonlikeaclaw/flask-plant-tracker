ce658128d4514e4c40ce743afff0dc59ab0104f2
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-03-02 22:08:50 -0500

    implement user object with database functionality

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login_manager
+
+
+class User(UserMixin, db.Model):
+    """Represents a User object"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(120), unique=True)
+    password_hash = db.Column(db.String(128))
+    plants = db.relationship("Plant", backref="owner", lazy="dynamic")
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
