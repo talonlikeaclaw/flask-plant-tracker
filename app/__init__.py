0a689100974074f5db6aa6471ebf9b0a92244ccc
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-03-02 19:09:21 -0500

    implement initialization

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from config import Config
+
+# Intialize extensions
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+
+def create_app(config_class=Config):
+    # Create and configure the app
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Initialize extensions with the app
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+    # Register blueprints
+    from app.routes import main_bp
+    app.register_blueprint(main_bp)
+
+    return app
